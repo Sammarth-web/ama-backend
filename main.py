994899150594bd351d5622ecf269dc32bfe3ed29
samarth yadav <121474767+Sammarth-web@users.noreply.github.com>
2025-07-01 15:42:36 +0530

    Uploaded project files

---
+++ main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, Query
+from fastapi.responses import HTMLResponse
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+import markdown
+
+# Load environment variables from .env
+load_dotenv()
+
+# Initialize the Gemini model
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+# Initialize FastAPI app
+app = FastAPI()
+
+
+SYSTEM_PROMPT = """
+    You are an expert AI assistant, Please format your reponse in **markdown**, using
+    --Heading (##)
+    --Bullet Points
+    --Bold/itallic text
+    --Code blocks Example
+"""
+
+@app.get("/", response_class=HTMLResponse)
+async def query_model(prompt: str = Query(..., description="The prompt to send to Gemini")):
+    try:
+        # Run the model with the given prompt
+        full_prompt = f"{SYSTEM_PROMPT.strip()}\n\n{prompt.strip()}"
+        result = model.invoke(full_prompt)
+
+        html_content = markdown.markdown(result.content) # type: ignore
+        
+        # Return result content in a basic HTML format
+        return f"""
+        <html>
+            <head><title>Gemini Response</title></head>
+            <body>
+                <h2>Prompt:</h2>
+                <p>{prompt}</p>
+                <h2>Response:</h2>
+                <p>{ html_content }</p>
+
+                <p>{ result }</p>
+            </body>
+        </html>
+        """
+    except Exception as e:
+        return HTMLResponse(content=f"<h2>Error:</h2><p>{str(e)}</p>", status_code=500)
+    
+
+
+@app.get("/model", response_class=HTMLResponse)
+async def query_model(prompt: str = Query(..., description="The prompt to send to Gemini")):
+    try :
+        
+        # Return result content in a basic HTML format
+        return f"""
+        <html>
+            <head><title>Gemini Response</title></head>
+            <body>
+                <h2> Hello from </h2>
+                <p> Deployments </p>
+            </body>
+        </html>
+        """
+    except Exception as e:
+        return HTMLResponse(content=f"<h2>Error:</h2><p>{str(e)}</p>", status_code=500)
